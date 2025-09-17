318821f0aef6ae62a822f39a8c55ab45dc2fd00f
Matthew-ljm <huifeidexiaogougou@126.com>
2025-09-17 19:52:56 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vnfxyospxnevclsctzpg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuZnh5b3NweG5ldmNsc2N0enBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMDc2NzEsImV4cCI6MjA3MzY4MzY3MX0.KlTgRsJrv4Slby4WEw3MszIuI1qrtXWb1zyjyWNJ-WA';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
